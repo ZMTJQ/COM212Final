460263bd390c276b185262d8544e7c1f5ccbfa13
EvanL77 <150301463+EvanL77@users.noreply.github.com>
2024-04-29 21:11:46 -0400

    Add files via upload

---
+++ MovieHash.java
@@ -0,0 +1,95 @@
+
+public class MovieHash{
+	private MovieNode[] h = new MovieNode[251];
+	private int n = 0;
+	
+	public int hash(int k){
+		return k%251;
+	}
+	public boolean empty(){
+		if(n==0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public void insert(MovieNode x){
+		int temp = hash(x.getID());
+		if(h[temp]==null){
+			h[temp]=x;
+			n++;
+			
+		}
+		else{
+			MovieNode head = h[temp];
+	 		h[temp] = x;
+	 		h[temp].setRight(head);
+	 		n++;
+	 	}
+	}
+	
+	public void delete(int k){
+		int num = hash(k);
+		MovieNode temp = h[num];
+		if(temp!=null){
+			if(temp.getID()==k){
+				h[num] = temp.getRight();
+				n--;
+		
+			}
+		
+			else{
+				while(temp.getRight()!=null){
+					if(k==temp.getRight().getID()){
+						MovieNode temp2 = temp.getRight();
+						temp.setRight(temp2.getRight());
+						temp2.setRight(null);
+						n--;
+					}
+					else{
+						temp = temp.getRight();
+					}
+			
+				}
+		
+			}
+		
+		}
+	}
+	
+	public void print(){
+	System.out.println("Movies: "+n);
+	for(int i = 0; i<251; i++){
+		System.out.print(i+": ");
+		MovieNode temp = h[i];
+		while(temp!=null){
+			System.out.print(temp.getID()+": ");
+			temp = temp.getRight();
+			
+		}
+			System.out.println(temp);
+		
+	}
+	
+	}
+	
+	public MovieNode lookUp(int k){
+		int key = hash(k);
+		MovieNode temp = h[key];
+		while(temp!=null)
+		{
+			if(temp.getID()==k){
+				return temp;
+			}
+			else{
+				temp = temp.getRight();
+			}
+		}
+		return null;
+		
+	}
+	
+	
+}
