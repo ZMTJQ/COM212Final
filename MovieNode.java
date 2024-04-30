71946b87a3fc2c4eadbd9f3cdc1d019cfa52b5dd
Arjun Premkumar <65966255+Arjunprem@users.noreply.github.com>
2024-04-29 20:24:13 -0400

    Add files via upload

---
+++ MovieNode.java
@@ -0,0 +1,53 @@
+
+
+public class MovieNode {
+
+	
+	private String name;
+	private int ReleaseDate;
+	private int UniqueIdCode;
+	private int RottenTomatoes;
+	private MovieNode left;
+	private MovieNode right;
+
+	public MovieNode(String name0,int releasedate0, int uniqueidcode0, int rottenTomatoes0) {
+		name = name0;
+		ReleaseDate= releasedate0%100000000;
+		UniqueIdCode=uniqueidcode0%100000;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getReleaseDate(){
+		return ReleaseDate;
+	}
+
+	public int getID() {
+		return UniqueIdCode;
+	}
+
+	public int RottenTomatoes() {
+		return RottenTomatoes;
+	}
+	public void setLeft(MovieNode left){
+		this.left = left;
+	}
+	public void setRight(MovieNode right){
+		this.right = right;
+	}
+	public MovieNode getRight(){
+		return this.right;
+	}
+	public MovieNode getLeft(){
+		return this.left;
+	}
+
+	
+
+	
+}
+
+
+
